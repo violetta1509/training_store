f7da41f3cfaca640066d7ac65f3c938110d829fb
WoLkErSs <ivanpauls1705@gmail.com>
2019-06-04 09:53:45 +0300

    Added auntetication with FB

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,9 +21,11 @@ require 'pry'
   end
 
   def self.from_omniauth(auth)
-    where(email: auth.info.email).first_or_create! do |user|
+    where(provider: auth.provider, uid: auth.uid).or(where(email: auth.info.email)).first_or_create! do |user|
       # provider: auth.provider, uid: auth.uid OR
-      # user.email = auth.info.email
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
